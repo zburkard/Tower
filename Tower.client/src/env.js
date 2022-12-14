20c47186befd69083a3119ea6493cc58f332829b
Zak Burkard <zbtoyota@gmail.com>
2022-12-13 19:11:41 -0700

    commit

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'zburkard.us.auth0.com'
+export const clientId = 'oribbDAX7rvqmLijDKvEKembEg6GuFwc'
+export const audience = 'https://zak-sandbox.com'
---
+++ Tower/server/models/TowerEvent.js
@@ -0,0 +1,5 @@
+import { Schema } from "mongoose";
+
+export const EventSchema = new Schema{
+
+}
